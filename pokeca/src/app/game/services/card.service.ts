4529267de06a9a6e85abb5a7acd99290d4fd095a
kenzi ueuchi <ueuchikenji@ken3916.local>
2026-02-18 03:34:46 +0900

    デプロイ用修正

--- pokeca/src/app/game/services/card.service.ts
+++ pokeca/src/app/game/services/card.service.ts
@@ -2,15 +2,16 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { PokemonCard } from '../../models/card.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CardService {
   private http = inject(HttpClient);
-
   // APIのベースURL（環境設定から取得）
-  private apiUrl = 'http://localhost:8000/api/cards';
+  // private apiUrl = 'http://localhost:8000/api/cards';
+  private apiUrl = environment.apiUrl;
 
   /**
    * カード一覧を取得
